c3f71d3d793ac29f6055a42b8260ffe99aaaf7f2
Chloe Stars <chloe@meow-mini.local>
2016-08-04 14:25:44 -0700

    Initial Commit

---
+++ Collage/CollageView.m
@@ -0,0 +1,52 @@
+//
+//  CollageView.m
+//  Collage
+//
+//  Created by Chloe Stars on 8/4/16.
+//  Copyright © 2016 Chloe Stars. All rights reserved.
+//
+
+#import "CollageView.h"
+
+@implementation CollageView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
